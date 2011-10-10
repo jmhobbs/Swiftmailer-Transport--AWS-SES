2ce9b80f0140a344b3b46906774c8096a736ba96
John Hobbs <john@velvetcache.org>
2011-10-10 10:58:37 -0500

    Update example code.

--- example.php
+++ example.php
@@ -1,9 +1,6 @@
 <?php
-	// Defining this constant will make the AWS Transport
-	// dump the XML recieved from AWS.
-	//define( 'SWIFT_AWS_DEBUG', true );
-
 	require_once 'lib/swift_required.php';
+	require_once 'Transport_AWSTransport.php';
 	require_once 'AWSTransport.php';
 	require_once 'AWSInputByteStream.php';
 
@@ -12,16 +9,17 @@
 
 	//Create the Transport
 	$transport = Swift_AWSTransport::newInstance( AWSAccessKeyId, AWSSecretKey );
+	$transport->setDebug( true ); // Print's the response from AWS for debugging.
 
 	//Create the Mailer using your created Transport
-	$mailer = Swift_Mailer::newInstance($transport);
+	$mailer = Swift_Mailer::newInstance( $transport );
 
 	//Create the message
 	$message = Swift_Message::newInstance()
-	->setSubject("What up?")
-	->setFrom(array('from@domain.com'))
-	->setTo(array('to@domain.com'))
-	->setBody("<p>Dude, I'm <b>totally</b> sending you email via AWS.</p>", 'text/html')
-	->addPart("Dude, I'm _totally_ sending you email via AWS.", 'text/plain');
+	->setSubject( 'What up?' )
+	->setFrom( array( 'from@domain.com' ) )
+	->setTo( array( 'to@domain.com' ) )
+	->setBody( "<p>Dude, I'm <b>totally</b> sending you email via AWS.</p>", 'text/html' )
+	->addPart( "Dude, I'm _totally_ sending you email via AWS.", 'text/plain' );
 
 	echo "Sent: " . $mailer->send( $message ) . "\n";
