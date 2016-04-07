6ce4e23962b55b80762f89df59191d28628c5671
John Hobbs <john@velvetcache.org>
2016-04-06 22:57:47 -0500

    Add option to reuse socket between sends.

--- example/send-multiple.php
+++ example/send-multiple.php
@@ -6,11 +6,12 @@
 	 * 2. Copy config.php.example to config.php and add your AWS credentials
 	 * 3. Run this script!
 	 */
-	require_once(__DIR__ . '/../vendor/autoload.php');
+  require_once(__DIR__ . '/../vendor/autoload.php');
 	require_once('./config.php');
 
 	$transport = Swift_AWSTransport::newInstance( AWSAccessKeyId, AWSSecretKey );
 	$transport->setDebug( true ); // Print the response from AWS to the error log for debugging.
+  $transport->setPersistent( true ); // Reuse the socket
 
 	//Create the Mailer using your created Transport
 	$mailer = Swift_Mailer::newInstance( $transport );
@@ -23,10 +24,26 @@
 	->setBody( "<p>Dude, I'm <b>totally</b> sending you email via AWS.</p>", 'text/html' )
 	->addPart( "Dude, I'm _totally_ sending you email via AWS.", 'text/plain' );
 
-	echo "Sending\n\n";
-	echo "From: " . FROM_ADDRESS . "\n";
-	echo "  To: " . TO_ADDRESS . "\n";
+  echo "Sending\n\n";
+  echo "From: " . FROM_ADDRESS . "\n";
+  echo "  To: " . TO_ADDRESS . "\n";
+	try {
+		echo "Sent: " . $mailer->send( $message ) . "\n";
+	}
+	catch( AWSEmptyResponseException $e ) {
+		echo $e . "\n";
+  }
 
+	$message = Swift_Message::newInstance()
+	->setSubject( 'Testing Swiftmailer SES AGAIN!' )
+	->setFrom( array( FROM_ADDRESS ) )
+	->setTo( array( TO_ADDRESS ) )
+	->setBody( "<p>Dude, I'm <b>totally</b> sending you email via AWS and the SAME SOCKET.</p>", 'text/html' )
+	->addPart( "Dude, I'm _totally_ sending you email via AWS and the SAME SOCKET.", 'text/plain' );
+
+  echo "Sending\n\n";
+  echo "From: " . FROM_ADDRESS . "\n";
+  echo "  To: " . TO_ADDRESS . "\n";
 	try {
 		echo "Sent: " . $mailer->send( $message ) . "\n";
 	}
